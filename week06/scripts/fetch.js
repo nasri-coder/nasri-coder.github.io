3f6c6621bf654fedcecaa30081131dd531528968
nasri-coder <nasrihashim644@mail.com>
2020-12-15 20:50:24 +0800

    updated

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -5,7 +5,18 @@ function GetBookings() {
         .then(json => {
         // Do something with the data
         console.log(json.bookings);
-        });
+        let bookingList = document.getElementById("bookingList");
+        bookingList.innerHTML = "";
+        for (let i = 0; i < json.bookings.length; i++) {
+            let gName = json.bookings[i].name;
+            let gEmail = json.bookings[i].email;
+            let gPax = json.bookings[i].pax;
+            let gId = json.bookings[i].id;
+
+            bookingList.innerHTML += gId + "-" + gName + ", " +
+                gEmail + ", pax:" + gpax + "<br>";
+        }
+    });
     }
 
     let getBookingBtn = document.getElementById("getBooking");
